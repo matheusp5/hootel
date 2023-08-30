f3c659e6bbb70cdc5517b1342a0eb12bda03ed81
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-30 15:53:25 -0300

    feat(hotels): hotel controller implementation

--- Hootel.Hotels/Controllers/HotelController.cs
+++ Hootel.Hotels/Controllers/HotelController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hootel.Hotels.Repositories.Interfaces;
+using Hootel.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hootel.Hotels.Controllers;
 
@@ -6,5 +8,39 @@ namespace Hootel.Hotels.Controllers;
 [ApiController]
 public class HotelController : ControllerBase
 {
-    
+    private readonly IHotelsRepository _hotelsRepository;
+
+    public HotelController(IHotelsRepository hotelsRepository)
+    {
+        _hotelsRepository = hotelsRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var hotels = await _hotelsRepository.Get();
+        return Ok(hotels);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Hotel([FromRoute] int id)
+    {
+        var hotel = await _hotelsRepository.Get(id);
+        return Ok(hotel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] Hotel hotel)
+    {
+        await _hotelsRepository.Save(hotel);
+        return StatusCode(201);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Remove([FromRoute] int id)
+    {
+        var hotel = await _hotelsRepository.Get(id);
+        await _hotelsRepository.Delete(hotel);
+        return Ok();
+    }
 }
