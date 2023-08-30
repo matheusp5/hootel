de89075a45d90bafd110f83aa1c1612adefd6955
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-30 16:51:52 -0300

    feat(rooms): room migration

---
+++ Hootel.Rooms/Migrations/20230830195033_RoomsTableCreation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hootel.Rooms.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoomsTableCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Code = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    RoomNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    BathroomsNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    DailyPrice = table.Column<int>(type: "INTEGER", nullable: false),
+                    HotelId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
