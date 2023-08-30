242993b40fe0138c9925a08829c54969d2aa0925
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-30 15:45:07 -0300

    feat(hotels): hotel controller and hotels repository interface

---
+++ Hootel.Hotels/Controllers/HotelController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hootel.Hotels.Controllers;
+
+[Route("api/hotels")]
+[ApiController]
+public class HotelController : ControllerBase
+{
+    
+}
---
+++ Hootel.Hotels/Repositories/Interfaces/IHotelsRepository.cs
@@ -0,0 +1,11 @@
+﻿using Hootel.Models;
+
+namespace Hootel.Hotels.Repositories.Interfaces;
+
+public interface IHotelsRepository
+{
+    List<Hotel> Get();
+    Hotel Get(string id);
+    void Save(Hotel hotel);
+    void Delete(string id);
+}
