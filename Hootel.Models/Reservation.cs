5601b22b3abccc3d084b41e6628e1073f2bd62a3
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-30 18:06:55 -0300

    feat(models): reservation model

---
+++ Hootel.Models/Reservation.cs
@@ -0,0 +1,11 @@
+﻿namespace Hootel.Models;
+
+public class Reservation : BaseModel
+{
+    public DateTime ChekIn { get; set; }
+    public DateTime CheckOut { get; set; }
+    public decimal Total { get; set; }
+    public string Applicationuser { get; set; }
+    public int HotelId { get; set; }
+    public int RoomId { get; set; }
+}
