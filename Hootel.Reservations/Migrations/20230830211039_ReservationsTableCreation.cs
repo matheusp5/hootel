0d9d0628820bf7efd179f7a1bcfa815f9ab3741b
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-30 18:12:49 -0300

    feat(reservations): create table migration

---
+++ Hootel.Reservations/Migrations/20230830211039_ReservationsTableCreation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hootel.Reservations.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReservationsTableCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ChekIn = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CheckOut = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Total = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Applicationuser = table.Column<string>(type: "TEXT", nullable: false),
+                    HotelId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RoomId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+        }
+    }
+}
