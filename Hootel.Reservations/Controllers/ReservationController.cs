ad31c48e2710b7a17325158d4ccdd6a2d7b1e12b
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-30 18:17:52 -0300

    feat(reservations): reservation controller

--- Hootel.Reservations/Controllers/ReservationController.cs
+++ Hootel.Reservations/Controllers/ReservationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hootel.Models;
+using Hootel.Reservations.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hootel.Reservations.Controllers;
 
@@ -6,5 +8,39 @@ namespace Hootel.Reservations.Controllers;
 [ApiController]
 public class ReservationController : ControllerBase
 {
+    private readonly IReservationsRepository _reservationsRepository;
+
+    public ReservationController(IReservationsRepository reservationsRepository)
+    {
+        _reservationsRepository = reservationsRepository;
+    }
     
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var hotels = await _reservationsRepository.Get();
+        return Ok(hotels);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Hotel([FromRoute] int id)
+    {
+        var hotel = await _reservationsRepository.Get(id);
+        return Ok(hotel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] Reservation reservation)
+    {
+        await _reservationsRepository.Save(reservation);
+        return StatusCode(201);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Remove([FromRoute] int id)
+    {
+        var hotel = await _reservationsRepository.Get(id);
+        await _reservationsRepository.Delete(hotel);
+        return Ok();
+    }
 }
