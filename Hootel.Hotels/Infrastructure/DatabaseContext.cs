1f4fa9f8a97f90a7366707b910a6b4df037b7d9c
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-30 15:34:36 -0300

    feat(models, hotels): add hotel class and create database context

---
+++ Hootel.Hotels/Infrastructure/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hootel.Hotels.Infrastructure;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Models.Hotel> Hotels { get; set; }
+}
---
+++ Hootel.Models/Hotel.cs
@@ -0,0 +1,11 @@
+﻿namespace Hootel.Models;
+
+public class Hotel : BaseModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Country { get; set; }
+    public string State { get; set; }
+    public string City { get; set; }
+    public string Address { get; set; }
+}
