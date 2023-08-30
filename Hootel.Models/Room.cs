5fc360b90b9883965e6fd611ccce0e806ba28836
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-30 15:59:53 -0300

    feat(models): create room model

---
+++ Hootel.Models/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace Hootel.Models;
+
+public class Room : BaseModel
+{
+    public string Code { get; set; }
+    public string Description { get; set; }
+    public int RoomNumber { get; set; }
+    public int BathroomsNumber { get; set; }
+    public int DailyPrice { get; set; }
+    public int HotelId { get; set; }
+}
