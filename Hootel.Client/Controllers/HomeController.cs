d1b75c85f025f747bb847bf406ad1095353cea77
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-31 17:22:46 -0300

    feat(client): search front-end implementation

--- Hootel.Client/Controllers/HomeController.cs
+++ Hootel.Client/Controllers/HomeController.cs
@@ -1,18 +1,22 @@
 ﻿using System.Diagnostics;
+using Hootel.Client.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Hootel.Client.Models;
 using Hootel.Client.Services.Interfaces;
 using Hootel.Client.ViewModel;
+using Hootel.Models;
 
 namespace Hootel.Client.Controllers;
 
 public class HomeController : Controller
 {
     private readonly IHotelService _hotelService;
+    private readonly IRoomService _roomService;
 
-    public HomeController(IHotelService hotelService)
+    public HomeController(IHotelService hotelService, IRoomService roomService)
     {
         _hotelService = hotelService;
+        _roomService = roomService;
     }
 
     public async Task<IActionResult> Index()
@@ -20,13 +24,58 @@ public class HomeController : Controller
         var hotels = await this._hotelService.GetAllHotels(5);
         return View(hotels);
     }
-    
+
     [HttpGet("buscar")]
-    public async Task<IActionResult> Search()
+    public async Task<IActionResult> Search([FromQuery] SearchDTO dto)
     {
-        return View();
+        ViewBag.AlreadySearch = false;
+        if (dto.state == null && dto.people == 0 && dto.city == null && dto.bath == 0 && dto.rooms == 0)
+        {
+            return View();
+        }
+
+        if (dto.state == null || dto.city == null)
+        {
+            return View();
+        }
+        
+        ViewBag.AlreadySearch = true;
+
+        var query = await _roomService.GetAllRooms();
+        var filteredRooms = new List<Room>();
+        var optionalFilteredRooms = new List<Room>();
+
+        // filtros obrigatórios
+        if (!string.IsNullOrEmpty(dto.state) && !string.IsNullOrEmpty(dto.city))
+        {
+            foreach (var room in query)
+            {
+                var hotel = await _hotelService.GetHotel(room.HotelId);
+                if (hotel != null && hotel.State == dto.state && hotel.City == dto.city)
+                {
+                    filteredRooms.Add(room);
+                }
+            }
+        }
+
+        // filtros opcionais
+        foreach (var room in filteredRooms)
+        {
+            if (dto.people == 0 || room.PeopleNumber >= dto.people)
+            {
+                if (dto.rooms == 0 || room.RoomNumber >= dto.rooms)
+                {
+                    if (dto.bath == 0 || room.BathroomsNumber >= dto.bath)
+                    {
+                        optionalFilteredRooms.Add(room);
+                    }
+                }
+            }
+        }
+
+        return View(optionalFilteredRooms);
     }
-    
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
---
+++ Hootel.Client/DTO/SearchDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Hootel.Client.DTO;
+
+// [FromQuery] int people, [FromQuery] int rooms, [FromQuery] int bath, [FromQuery] string state, [FromQuery] string city
+public class SearchDTO
+{
+    public int people { get; set; }
+    public int rooms { get; set; }
+    public int bath { get; set; }
+    public string state { get; set; }
+    public string city { get; set; }
+}
