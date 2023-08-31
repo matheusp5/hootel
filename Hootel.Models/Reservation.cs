e5c5a17bc9c6aaa5cb3911228a228d095598940e
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-30 23:06:20 -0300

    refact(models): ApplicationUser name format

--- Hootel.Models/Reservation.cs
+++ Hootel.Models/Reservation.cs
@@ -5,7 +5,7 @@ public class Reservation : BaseModel
     public DateTime ChekIn { get; set; }
     public DateTime CheckOut { get; set; }
     public decimal Total { get; set; }
-    public string Applicationuser { get; set; }
+    public string ApplicationUser { get; set; }
     public int HotelId { get; set; }
     public int RoomId { get; set; }
 }
