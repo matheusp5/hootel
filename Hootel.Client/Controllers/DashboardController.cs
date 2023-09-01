57072708a47a69f9f14d970be4f527864ed4e20c
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-09-01 12:31:40 -0300

    feat(client): initial dashboard controller

---
+++ Hootel.Client/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hootel.Client.Controllers;
+
+public class DashboardController : Controller
+{
+    public async Task<IActionResult> Dashboard()
+    {
+        if (User.IsInRole("admin"))
+        {
+            
+            return View("Admin");
+        }
+
+        return View("Client");
+    }
+}
