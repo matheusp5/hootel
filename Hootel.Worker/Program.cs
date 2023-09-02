61af719b4b62272f77e11419a388c9fd6cea9e28
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-09-02 01:43:30 -0300

    build(worker): create background worker service

---
+++ Hootel.Worker/Program.cs
@@ -0,0 +1,7 @@
+using Hootel.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+host.Run();
---
+++ Hootel.Worker/Worker.cs
@@ -0,0 +1,12 @@
+namespace Hootel.Worker;
+
+public class Worker : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
